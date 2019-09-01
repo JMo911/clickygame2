afab97581141e95064106555e190a05b68786afe
Jacob Moss <jacobmoss09@gmail.com>
2019-08-31 22:02:37 -0500

    test

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/wrapper'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Jacob's title</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jacob's React
-        </a>
-      </header>
-    </div>
+    <Wrapper name='Angela'></Wrapper>
   );
 }
 
